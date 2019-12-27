e1fa446ce55990c914cfb13567c3fb371faf897b
Ovi Savescu <ovi@ovswebsites.com>
2019-12-27 13:41:09 +0200

    updated homepage section on swim lessons

--- web/src/components/hp-sections/swim-lessons.js
+++ web/src/components/hp-sections/swim-lessons.js
@@ -4,7 +4,7 @@ import {jsx, Styled} from 'theme-ui'
 import {Link, useStaticQuery, graphql} from 'gatsby'
 import Img from 'gatsby-image'
 
-import {FaHamburger, FaPhone} from 'react-icons/fa'
+import {FaSwimmer} from 'react-icons/fa'
 
 // containers
 import SectionWithSidebar from '../../containers/section-with-sidebar'
@@ -18,19 +18,19 @@ const FoodSection = () => {
 }
 
 const MainContent = () => {
-  const {foodImage} = useStaticQuery(
-    graphql`
-    query {
-      foodImage: file(relativePath: { eq: "lunch-time-illustration.png" }) {
-        childImageSharp {
-          fluid(maxWidth: 726) {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-        }
-      }
-    }
-    `
-  )
+  // const {foodImage} = useStaticQuery(
+  //   graphql`
+  //   query {
+  //     foodImage: file(relativePath: { eq: "lunch-time-illustration.png" }) {
+  //       childImageSharp {
+  //         fluid(maxWidth: 726) {
+  //           ...GatsbyImageSharpFluid_tracedSVG
+  //         }
+  //       }
+  //     }
+  //   }
+  //   `
+  // )
   return (
     <div sx={{
       // mx: [0, 5, 4, 5, 6]
@@ -44,11 +44,11 @@ const MainContent = () => {
 const Sidebar = () => {
   return (
     <div sx={{p: {lineHeight: 'snug'}}}>
-      <Styled.h1 as='h2'>Lunch Provided</Styled.h1>
-      <p>To make your life a little bit easier, a nutritious and delicious lunch is provided daily. </p>
-      <p>A vegetarian option is always available. Little ones get a morning snack and everyone enjoys an afternoon snack of ice cream or ice pops.</p>
+      <Styled.h1 as='h2'>Swim Lessons</Styled.h1>
+      <p>Camp Laurelwood is a great place for your child to learn to swim!  We have a beautiful lake, 2 pools, and lots of opportunity for campers to participate in aquatics programming.  </p>
+      <p>In addition to recreational swimming, campers in grades K-7 have instructional swim approximately 5 times throughout the week.  Our qualified instructors teach them on their level, and provide safe and fun activities to promote swim development.</p>
       <Link to='/' sx={{variant: 'buttons.3D', display: 'inline-block', my: 3, mr: 3, position: 'relative', pl: '3.5rem'}}>
-        <FaHamburger sx={{position: 'absolute', top: '0.7rem', left: '1rem', fontSize: 4}} /> Sample Lunch Menu
+        <FaSwimmer sx={{position: 'absolute', top: '1rem', left: '1rem', fontSize: 4}} /> Our Swimming Program
       </Link>
       {/* <a href='tel:2034213736' sx={{variant: 'buttons.3DAccent', display: 'inline-block'}}><FaPhone /> Call Us: (203) 421-3736</a> */}
     </div>
